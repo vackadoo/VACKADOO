226d5717b4137ceb7ab91d97a0bca35f35abc6ee
Satyam <satyamjha1607@gmail.com>
2024-09-05 23:54:43 +0530

    IOS support added

--- src/pages/clientpage/FiemFoundation.jsx
+++ src/pages/clientpage/FiemFoundation.jsx
@@ -110,6 +110,7 @@ const FiemFoundation = () => {
                   className="object-contain rounded-xl md:max-h-[300px] max-h-[250px]  drop-shadow-xl"
                   loop
                   muted
+                  playsInline // Added playsInline attribute
                   ref={videoRefs[index + 1]}
                   onMouseOver={() => handleMouseOver(videoRefs[index + 1])}
                   onMouseLeave={() => handleMouseLeave(videoRefs[index + 1])}
