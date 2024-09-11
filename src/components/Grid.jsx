ed2780ad767b8cb63f602fd21c51f73182e5ae24
piyush8512 <prajapatipiyush851@gmail.com>
2024-09-11 14:03:22 +0530

    Updated Service page with sfvideo

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -162,10 +162,96 @@
 
 // export default LandingPage;
 
+// import React, { useEffect, useRef } from "react";
+// import HeroSection from "./HeroSection";
+// import arrow from "../assets/images/downarraow.png";
+// // import intro from "../assets/video/intro.mov";
+
+// const LandingPage = () => {
+//   const intro =
+//     "https://res.cloudinary.com/dlanlvnce/video/upload/v1724911066/qvhrpnxhsaowtzraeoe9.mov";
+//   const videoRef = useRef(null);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrollTop = window.scrollY;
+//       const video = videoRef.current;
+
+//       if (video) {
+//         // Pause the video if the scroll position is past the video height
+//         if (scrollTop > video.offsetHeight) {
+//           video.pause();
+//         } else {
+//           video.play();
+//         }
+
+//         // Adjust the video opacity based on scroll position
+//         video.style.opacity = Math.max(0.2, 1 - scrollTop / 900);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     // Cleanup the event listener on component unmount
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   const scrollToNextSection = () => {
+//     const nextSection = document.getElementById("next-section");
+//     nextSection.scrollIntoView({ behavior: "smooth" });
+//   };
+
+//   return (
+//     <div>
+//       {/* Video Background Section */}
+//       <section className="relative h-[50vh] md:h-[80vh] flex justify-center items-center overflow-hidden">
+//         <a href="https://youtu.be/X5YmbXSpVoU">
+//           <div className="absolute inset-0 flex justify-center items-center md:pt-1 pt-8">
+//             <video
+//               ref={videoRef}
+//               className="md:max-w-[1200px] md:h-full object-cover"
+//               autoPlay
+//               loop
+//               muted
+//               playsInline
+//             >
+//               <source src={intro} type="video/mp4" />
+//             </video>
+//           </div>
+//         </a>
+//         <div className="absolute inset-0 bg-black opacity-20"></div>
+//       </section>
+
+//       {/* Arrow Below the Video */}
+//       <div className="flex justify-center md:mt-1 mt-2">
+//         <button
+//           onClick={scrollToNextSection}
+//           className="hidden md:block text-white text-4xl animate-bounce mb-20"
+//         >
+//           <img src={arrow} alt="Scroll Down Arrow" />
+//         </button>
+//       </div>
+
+//       {/* Next Section */}
+//       <section
+//         id="next-section"
+//         className="md:h-[70vh] h-screen bg-black flex items-center justify-center"
+//       >
+//         <HeroSection />
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default LandingPage;
+
+
+
 import React, { useEffect, useRef } from "react";
 import HeroSection from "./HeroSection";
 import arrow from "../assets/images/downarraow.png";
-// import intro from "../assets/video/intro.mov";
 
 const LandingPage = () => {
   const intro =
@@ -216,6 +302,8 @@ const LandingPage = () => {
               loop
               muted
               playsInline
+              controls={false} // Add this to disable the native iOS full-screen controls
+              preload="auto"   // Ensure the video is preloaded
             >
               <source src={intro} type="video/mp4" />
             </video>
