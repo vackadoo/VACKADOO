35768d45f198cf82052b03bd16d0784cf8f5c26a
piyush8512 <prajapatipiyush851@gmail.com>
2024-09-15 21:06:25 +0530

    Your commit messavideo changee

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -247,91 +247,94 @@
 
 // export default LandingPage;
 
-// import React, { useEffect, useRef } from "react";
-// import HeroSection from "./HeroSection";
-// import arrow from "../assets/images/downarraow.png";
+import React, { useEffect, useRef } from "react";
+import HeroSection from "./HeroSection";
+import arrow from "../assets/images/downarraow.png";
 
-// const LandingPage = () => {
-//   const intro =
-//     "https://res.cloudinary.com/dlanlvnce/video/upload/v1724911066/qvhrpnxhsaowtzraeoe9.mov";
-//   const videoRef = useRef(null);
+const LandingPage = () => {
+  // const intro =
+  //   "https://res.cloudinary.com/dlanlvnce/video/upload/v1724911066/qvhrpnxhsaowtzraeoe9.mov";
 
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       const scrollTop = window.scrollY;
-//       const video = videoRef.current;
+  const intro =
+    "https://res.cloudinary.com/dlanlvnce/video/upload/v1724910824/samples/dance-2.mp4";
+  const videoRef = useRef(null);
 
-//       if (video) {
-//         // Pause the video if the scroll position is past the video height
-//         if (scrollTop > video.offsetHeight) {
-//           video.pause();
-//         } else {
-//           video.play();
-//         }
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const video = videoRef.current;
 
-//         // Adjust the video opacity based on scroll position
-//         video.style.opacity = Math.max(0.2, 1 - scrollTop / 900);
-//       }
-//     };
+      if (video) {
+        // Pause the video if the scroll position is past the video height
+        if (scrollTop > video.offsetHeight) {
+          video.pause();
+        } else {
+          video.play();
+        }
 
-//     window.addEventListener("scroll", handleScroll);
+        // Adjust the video opacity based on scroll position
+        video.style.opacity = Math.max(0.2, 1 - scrollTop / 900);
+      }
+    };
 
-//     // Cleanup the event listener on component unmount
-//     return () => {
-//       window.removeEventListener("scroll", handleScroll);
-//     };
-//   }, []);
+    window.addEventListener("scroll", handleScroll);
 
-//   const scrollToNextSection = () => {
-//     const nextSection = document.getElementById("next-section");
-//     nextSection.scrollIntoView({ behavior: "smooth" });
-//   };
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-//   return (
-//     <div>
-//       {/* Video Background Section */}
-//       <section className="relative h-[50vh] md:h-[80vh] flex justify-center items-center overflow-hidden">
-//         <a href="https://youtu.be/X5YmbXSpVoU">
-//           <div className="absolute inset-0 flex justify-center items-center md:pt-1 pt-8">
-//             <video
-//               ref={videoRef}
-//               className="md:max-w-[1200px] md:h-full object-cover"
-//               autoPlay
-//               loop
-//               muted
-//               playsInline
-//               controls={false} // Add this to disable the native iOS full-screen controls
-//               preload="auto"   // Ensure the video is preloaded
-//             >
-//               <source src={intro} type="video/mp4" />
-//             </video>
-//           </div>
-//         </a>
-//         <div className="absolute inset-0 bg-black opacity-20"></div>
-//       </section>
+  const scrollToNextSection = () => {
+    const nextSection = document.getElementById("next-section");
+    nextSection.scrollIntoView({ behavior: "smooth" });
+  };
 
-//       {/* Arrow Below the Video */}
-//       <div className="flex justify-center md:mt-1 mt-2">
-//         <button
-//           onClick={scrollToNextSection}
-//           className="hidden md:block text-white text-4xl animate-bounce mb-20"
-//         >
-//           <img src={arrow} alt="Scroll Down Arrow" />
-//         </button>
-//       </div>
+  return (
+    <div>
+      {/* Video Background Section */}
+      <section className="relative h-[50vh] md:h-[80vh] flex justify-center items-center overflow-hidden">
+        <a href="https://youtu.be/X5YmbXSpVoU">
+          <div className="absolute inset-0 flex justify-center items-center md:pt-1 pt-8">
+            <video
+              ref={videoRef}
+              className="md:max-w-[1200px] md:h-full object-cover"
+              autoPlay
+              loop
+              muted
+              playsInline
+              controls={false} // Add this to disable the native iOS full-screen controls
+              preload="auto" // Ensure the video is preloaded
+            >
+              <source src={intro} type="video/mp4" />
+            </video>
+          </div>
+        </a>
+        <div className="absolute inset-0 bg-black opacity-20"></div>
+      </section>
 
-//       {/* Next Section */}
-//       <section
-//         id="next-section"
-//         className="md:h-[70vh] h-screen bg-black flex items-center justify-center"
-//       >
-//         <HeroSection />
-//       </section>
-//     </div>
-//   );
-// };
+      {/* Arrow Below the Video */}
+      <div className="flex justify-center md:mt-1 mt-2">
+        <button
+          onClick={scrollToNextSection}
+          className="hidden md:block text-white text-4xl animate-bounce mb-20"
+        >
+          <img src={arrow} alt="Scroll Down Arrow" />
+        </button>
+      </div>
 
-// export default LandingPage;
+      {/* Next Section */}
+      <section
+        id="next-section"
+        className="md:h-[70vh] h-screen bg-black flex items-center justify-center"
+      >
+        <HeroSection />
+      </section>
+    </div>
+  );
+};
+
+export default LandingPage;
 
 // import React, { useEffect, useRef, useState } from "react";
 // import HeroSection from "./HeroSection";
@@ -433,79 +436,69 @@
 
 // export default LandingPage;
 
-import React, { useEffect, useState } from "react";
-import HeroSection from "./HeroSection";
-import arrow from "../assets/images/downarraow.png";
+// import React, { useEffect, useState } from "react";
+// import HeroSection from "./HeroSection";
+// import arrow from "../assets/images/downarraow.png";
 
-const LandingPage = () => {
-  const [videoId, setVideoId] = useState("");
+// const LandingPage = () => {
+//   const [videoId, setVideoId] = useState("");
 
-  useEffect(() => {
-    const fetchYouTubeVideo = async () => {
-      const apiKey = "AIzaSyD8v2dEVujXuN9jESTE0DTGPKZgEvrnw94";
-      const channelId = "UCoe-fi5V1y4laZOSjMDSGDA";
-      const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=1&order=date&type=video&key=${apiKey}`;
+//   useEffect(() => {
+//     const fetchYouTubeVideo = async () => {
+//       const apiKey = "AIzaSyD8v2dEVujXuN9jESTE0DTGPKZgEvrnw94";
+//       const channelId = "UCoe-fi5V1y4laZOSjMDSGDA";
+//       const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=1&order=date&type=video&key=${apiKey}`;
 
-      try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        if (data.items && data.items.length > 0) {
-          setVideoId(data.items[0].id.videoId);
-        }
-      } catch (error) {
-        console.error("Error fetching YouTube video:", error);
-      }
-    };
+//       try {
+//         const response = await fetch(apiUrl);
+//         const data = await response.json();
+//         if (data.items && data.items.length > 0) {
+//           setVideoId(data.items[0].id.videoId);
+//         }
+//       } catch (error) {
+//         console.error("Error fetching YouTube video:", error);
+//       }
+//     };
 
-    fetchYouTubeVideo();
-  }, []);
+//     fetchYouTubeVideo();
+//   }, []);
 
-  const scrollToNextSection = () => {
-    const nextSection = document.getElementById("next-section");
-    nextSection.scrollIntoView({ behavior: "smooth" });
-  };
+//   const scrollToNextSection = () => {
+//     const nextSection = document.getElementById("next-section");
+//     nextSection.scrollIntoView({ behavior: "smooth" });
+//   };
 
-  return (
-    <div>
-      {/* Video Background Section */}
-      <section className="relative h-[50vh] md:h-[80vh] flex justify-center items-center overflow-hidden">
-        <div className="absolute inset-0 flex justify-center items-center md:pt-1 pt-8 w-full">
-          {videoId && (
-            <iframe
-              id="youtube-player"
-              className="w-full h-full"
-              muted
-              loop
-              playsInline
-              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0&playsinline=1&rel=0`}
-              frameBorder="0"
-              allow="autoplay; encrypted-media"
-              allowFullScreen
-            ></iframe>
-          )}
-        </div>
-        <div className="absolute inset-0 bg-black opacity-20"></div>
-      </section>
+//   return (
+//     <div>
+//       {/* Video Background Section */}
+//       <section className="relative h-[50vh] md:h-[80vh] flex justify-center items-center overflow-hidden">
+//         <div className="absolute inset-0 flex justify-center items-center md:pt-1 pt-8 w-full">
+//           {videoId && (
+//             <iframe width="560" height="315" src="https://www.youtube.com/embed/Ro3DWrhQzcA?si=wkDmU5YlXT6x5e1v" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+//           )}
+//         </div>
+//         <div className="absolute inset-0 bg-black opacity-20"></div>
+//       </section>
 
-      {/* Arrow Below the Video */}
-      <div className="flex justify-center md:mt-1 mt-2">
-        <button
-          onClick={scrollToNextSection}
-          className="hidden md:block text-white text-4xl animate-bounce mb-20"
-        >
-          <img src={arrow} alt="Scroll Down Arrow" />
-        </button>
-      </div>
+//       {/* Arrow Below the Video */}
+//       <div className="flex justify-center md:mt-1 mt-2">
+//         <button
+//           onClick={scrollToNextSection}
+//           className="hidden md:block text-white text-4xl animate-bounce mb-20"
+//         >
+//           <img src={arrow} alt="Scroll Down Arrow" />
+//         </button>
+//       </div>
 
-      {/* Next Section */}
-      <section
-        id="next-section"
-        className="md:h-[70vh] h-screen bg-black flex items-center justify-center"
-      >
-        <HeroSection />
-      </section>
-    </div>
-  );
-};
+//       {/* Next Section */}
+//       <section
+//         id="next-section"
+//         className="md:h-[70vh] h-screen bg-black flex items-center justify-center"
+//       >
+//         <HeroSection />
+//       </section>
+//     </div>
+//   );
+// };
 
-export default LandingPage;
+// export default LandingPage;
